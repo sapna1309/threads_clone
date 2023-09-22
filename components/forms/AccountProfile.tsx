27030f45e46a286f3e6389d4b1e603f41d80cabf
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-09-22 19:22:30 +0530

    added

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+
+interface Props {
+    user: {
+      id: string;
+      objectId: string;
+      username: string;
+      name: string;
+      bio: string;
+      image: string;
+    };
+    btnTitle: string;
+  }
+const AccountProfile = ({user, btnTitle} : Props) => {
+  return (
+    <h1>AccountProfile</h1>
+  )
+}
+
+export default AccountProfile
